d459822ced67316ab3a6012dbbe6a6158ae04ec1
martrej <trejos.mariadelc@gmail.com>
2016-05-09 21:04:27 -0500

    prueba de versionamiento GITHUB

--- taller001/src/taller001/Taller001.java
+++ taller001/src/taller001/Taller001.java
@@ -15,7 +15,8 @@ public class Taller001 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // prueba de versionamiento GITHUB
+   
     }
     
 }
